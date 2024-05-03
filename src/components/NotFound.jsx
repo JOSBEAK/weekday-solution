f94a450c348c21f02a457244432a2948a4802112
ANURAG PANDEY <drecothea@ANURAGs-MacBook-Air.local>
2024-05-04 01:20:23 +0530

    Code cleanup - removed unnecessary code

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -4,9 +4,7 @@ const NotFound = () => {
   return (
     <div className="not-found">
       <img alt="not-found" src={NF} width="150" height="150"></img>
-      <p class="MuiTypography-root MuiTypography-body1 css-tbokyj">
-        No Jobs available for this category at the moment
-      </p>
+      <p>No Jobs available for this category at the moment</p>
     </div>
   );
 };
